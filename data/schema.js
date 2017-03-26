b98132f17698bf08d5015b3536baedc85692a36e
Jesper Johansson <jesper.johansson@strateg.se>
2017-03-26 21:03:45 +0200

    Added GraphQL functionality

---
+++ data/schema.js
@@ -0,0 +1,19 @@
+import { GraphQLSchema, GraphQLObjectType, GraphQLInt, GraphQLString } from 'graphql';
+
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: 'Query',
+    fields: () => ({
+      counter: {
+        type: GraphQLInt,
+        resolve: () => 42,
+      },
+      message: {
+        type: GraphQLString,
+        resolve: () => 'Hello World!',
+      },
+    }),
+  }),
+});
+
+export default schema;
--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 import express from 'express';
+import GraphQLHTTP from 'express-graphql';
 import { MongoClient } from 'mongodb';
+import schema from './data/Schema';
 
 let database;
 const app = express();
 const fallbackUrl = 'mongodb://localhost:27017/relay_test';
 
 app.use(express.static('public'));
+app.use('/graphql', GraphQLHTTP({
+  schema,
+}));
 
 MongoClient.connect(process.env.MONGO_URL || fallbackUrl, (err, db) => {
   if (err) throw err;
